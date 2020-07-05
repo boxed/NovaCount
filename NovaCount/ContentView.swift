f04e6b1c4f7bf1beb36e1c5f7839764dafbf4216
Anders Hovmöller <boxed@killingar.net>
2020-07-05 15:54:04 +0200

    First rough prototype

--- NovaCount/ContentView.swift
+++ NovaCount/ContentView.swift
@@ -9,13 +9,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    var numbers = -100...100
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .center) {
+            Text("⭐️⭐️").font(.system(size: 20))
+            Text("3 + 4").foregroundColor(Color.white).font(.system(size: 60))
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(self.numbers, id: \.self) { number in
+                        Text("\(number)")
+                            .foregroundColor(Color.white)
+                            .frame(width: 60, height: 70)
+                            .background(
+                          RoundedRectangle(cornerRadius: 4)
+                            .stroke(Color.purple)
+                        )
+                    }
+                }
+            }
+            .padding(.horizontal)
+            Button(action: {}) {
+                Text("Svara").foregroundColor(Color.white).padding()
+            }
+            .background(RoundedRectangle(cornerRadius: 4.0).stroke(Color.purple))
+        }
+        .background(Rectangle().fill(Color.black))
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+       
+
     }
 }
